c5c964a39e36a3e3715f7e85220bc67df99fbb62
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2023-07-24 18:26:47 +0200

    Get very basic structure in place

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+interface DOMElements {
+	[index: string]: HTMLElement | HTMLInputElement;
+}
+
+class NotrApp {
+    parent: HTMLElement;
+    $: DOMElements;
+
+    constructor(el: HTMLElement) {
+        this.parent = el;
+
+        this.$ = {
+            helloName: el.querySelector('[data-notr="hello-name"]') as HTMLElement,
+        }
+
+        this.setupUI();
+    }
+
+    setupUI(): void {
+        this.$.helloName.textContent = 'Adrian';
+    }
+}
+
+new NotrApp(document.body);
