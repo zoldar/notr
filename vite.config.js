150dc168ff4a6b3f2683617b2526e69a8178ea88
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2023-10-27 00:18:47 +0200

    Expose dev server on 127.0.0.1

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
   // Tauri expects a fixed port, fail if that port is not available
   server: {
     strictPort: true,
+    host: '127.0.0.1'
   },
   // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
   // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
